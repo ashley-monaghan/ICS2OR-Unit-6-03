8d288962b8ed1a41749bed56c237b02c1d969997
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-18 19:55:36 -0500

    sync

--- js/script.js
+++ js/script.js
@@ -21,28 +21,17 @@ if (navigator.serviceWorker) {
  */
 const getImage = async (URLAddress) => {
   try {
-    const result = await fetch(URLAddress)
+    const result = await fetch("http://openweathermap.org/img/wn/10d@2x.png")
     const jsonData = await result.json()
     console.log(jsonData)
     document.getElementById("api-image").innerHTML =
-      '<img src="' + jsonData.url + '" alt="API image" class="center" ' + ">"
-    if (jsonData.artist_url != "none") {
-      document.getElementById("image-artist").innerHTML =
-        "<p>Artist: " +
-        '<a href="' +
-        jsonData.artist_url +
-        '">' +
-        jsonData.artist +
-        "</a>"
-    } else {
-      document.getElementById("image-artist").innerHTML =
-        "<p>Artist: unknown</p>"
-    }
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
   } catch (err) {
     console.log(err)
   }
 }
 
-getImage(
-  "https://api.https://www.google.com/url?q=https%3A%2F%2Fopenweathermap.org%2Fweather-conditions&sa=D&sntz=1&usg=AOvVaw25gMqUGVU4FAcAo7HIPSZs.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-)
+getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
