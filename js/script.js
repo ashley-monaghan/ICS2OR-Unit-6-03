3ff53e51ddd961ba82c8805a8dc53e8728e89868
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-12-19 00:37:48 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -25,21 +25,19 @@ const getImage = async (URLAddress) => {
     const jsonData = await result.json()
     console.log(jsonData)
     document.getElementById("api-image").innerHTML =
-    '<img src="' + 
-      jsonData.url + 
-      '" alt="API image" class="center" ' +
-      '>'
+      '<img src="' + jsonData.url + '" alt="API image" class="center" ' + ">"
     if (jsonData.artist_url != "none") {
       document.getElementById("image-artist").innerHTML =
-      "<p>Artist: " +
-      '<a href="' +
-      jsonData.artist_url +
-      '">' +
-      jsonData.artist +
-      "</a>"
-  } else {
-    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
-  }
+        "<p>Artist: " +
+        '<a href="' +
+        jsonData.artist_url +
+        '">' +
+        jsonData.artist +
+        "</a>"
+    } else {
+      document.getElementById("image-artist").innerHTML =
+        "<p>Artist: unknown</p>"
+    }
   } catch (err) {
     console.log(err)
   }
