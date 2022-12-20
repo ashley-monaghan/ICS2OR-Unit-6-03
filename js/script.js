5095cb5ce407206b6f6078e40785d32562a2ef9c
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-20 16:57:51 -0500

    sync

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const weather2 = async (URLAddress) => {
 
     console.log(jsonData.weather)
     document.getElementById("image").innerHTML =
-      "<img src='http://api.openweathermap.org/data/2.5/weather?q=London" +
+      "<img src='http://openweathermap.org/img/wn/10d@2x.png" +
       image +
       "@2px.png' alt='Icon' width='10%'><br><h5>"
     ;(">")
