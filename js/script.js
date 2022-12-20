6022b063193a18f3e8f287baeee03f18925e2f8f
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-19 21:00:08 -0500

    sync

--- js/script.js
+++ js/script.js
@@ -19,30 +19,30 @@ if (navigator.serviceWorker) {
  * This function displays an alert.
  * Math
  */
-const getImage = async (URLAddress) => {
+const weather2 = async (URLAddress) => {
   try {
-    const result = await fetch(URLAddress)
-    const jsonData = await result.json()
-    var temperature1 = jsonDate.main.temp
-    var temperature2 = 0
-    const feeling = jsonDate.weather[0]
-    const image = feeling.icon
-    temp2 = temp1 - 273.15
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    var temperature1 = jsonDate.main.temp;
+    var temperature2 = 0;
+    const feeling = jsonDate.weather[0];
+    const image = feeling.icon;
+    temp2 = temp1 - 273.15;
 
-    console.log(jsonData.weather)
+    console.log(jsonData.weather);
     document.getElementById("Image").innerHTML =
-      "<img src='https://openweathermap.org/weather-conditions" +
+      "<img src='http://openweathermap.org/img/wn/10d@2x.png" +
       image +
       "@2px.png' alt='Icon' width='10%'><br><h5>"
-    ;(">")
+    ;(">");
 
     document.getElementById("TheWeather").innerHTML =
-      "The weather right now is" + temperature2.toFixed(2) + " °C"
+      "The weather right now is" + temperature2.toFixed(2) + " °C";
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
-getImage(
+weather2(
   "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
 )
