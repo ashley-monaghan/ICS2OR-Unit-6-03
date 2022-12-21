2ddefbd03f3883e09d90c905944c7785741fcabf
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-21 08:49:54 -0500

    changed format

--- js/script.js
+++ js/script.js
@@ -44,6 +44,7 @@ const weather2 = async (URLAddress) => {
     console.log(err)
   }
 }
+
 weather2(
   "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
 )
